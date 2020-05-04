ac0eb91af17c9bbca3dacf46f2694adbd50a1336
CasperGN <Casper_too@hotmail.com>
2020-05-04 12:43:26 -0400

    Included PyIntruder re-write into the code

--- fuzzer.py
+++ fuzzer.py
@@ -7,6 +7,11 @@ parser.add_argument('--threads', type=int, help='Number of threads to fuzz (defa
 parser.add_argument('--target', type=str, help='Single target to run against')
 parser.add_argument('--targets', type=str, help='File with newline seperator including several targets')
 parser.add_argument('--exclude', type=str, help='String of response codes seperated by "," (example 404,401,403) to exclude from printing')
+parser.add_argument('--proxy', type=str, help='Proxy to route through (example "http://localhost:8080")')
+parser.add_argument('--blocks', type=int, help='Amount of blocks registered to exit (default: 5). This means that if 5 blocks are registered the fuzzer will exit')
+parser.add_argument('--redir', help='Allow HTTP redirects', action="store_true")
+
+
 
 if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
@@ -19,5 +24,9 @@ callback = args.threads if args.threads else 5
 target = args.target if args.target else None
 targets = args.targets if args.targets else None
 exclude = args.exclude if args.exclude else None
+proxy = args.proxy if args.proxy else None
+blocks = args.blocks if args.blocks else 5
+redir = args.redir if args.redir else False
 
-oobfuzz = OOBFuzz(output, callback, target, targets, exclude)
+
+oobfuzz = OOBFuzz(output, callback, target, targets, exclude, proxy, blocks, redir)
--- oobfuzz/__init__.py
+++ oobfuzz/__init__.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 import requests
 import sys, concurrent.futures, re
-from PyIntruder import PyIntruder
 from datetime import datetime
 from subprocess import PIPE, Popen
-from os import devnull, walk
+from os import devnull, walk, path
 from urllib.parse import quote
-
+from urllib3 import disable_warnings
+from random import choice
+from time import sleep
+from socket import gaierror
+from OpenSSL import SSL
 
 class OOBFuzz():
 
-    def __init__(self, output, threads, target, targets, exclude):
+    def __init__(self, output, threads, target, targets, exclude, proxy, blocks, redir):
+
+        disable_warnings()
+
+        if proxy:
+            self.proxy = {"http": proxy, "https": proxy}
         self.output = output
         self.threads = threads
         self.targets = []
+        self.blocks = blocks
+        self.redir = redir
 
         if exclude:
             self.exclude = exclude.split(',')
@@ -25,16 +35,21 @@ class OOBFuzz():
         self.payloads = []
         self.excludedParams = ['submit']
 
-        self.id = str(datetime.now()).split(':')[0].replace(' ', 'T')
 
         (_, _, filenames) = next(walk('./payloads/'))
         for filename in filenames:
             with open(f'./payloads/{filename}', 'r') as f:
                 tmpDict = {filename[:filename.index(".")]: []}
-                for payload in f:                    
-                    tmpDict[filename[:filename.index(".")]].append(quote(payload.rstrip().replace('.collabxyz.', f'.{self.id}.collabxyz.')))
+                for payload in f:
+                    tmpDict[filename[:filename.index(".")]].append(quote(payload.rstrip()))
             self.payloads.append(tmpDict)
-        
+
+        self.useragents = []
+        pwd = path.dirname(path.realpath(__file__))
+        with open(f'{pwd}/data/user-agents.txt', 'r') as agents:
+            for useragent in agents:
+                self.useragents.append(useragent.rstrip())
+
         if targets:
             try:
                 with open(f'{targets}', 'r') as targetsFile:
@@ -63,8 +78,8 @@ class OOBFuzz():
         urls = list(set(urls))
         result = []
         
-        for url in urls:
-            matches = self.paramRE.findall(url)
+        for baseurl in urls:
+            matches = self.paramRE.findall(baseurl)
 
             for match in matches:
                 if str(match[1]).lower() in self.excludedParams:
@@ -72,11 +87,40 @@ class OOBFuzz():
                 value = match[2]
                 for d in self.payloads:
                     for attack, payloadList in d.items():
-                        intruder = PyIntruder(redir=True, save=False, out=False, url=url.replace(value, '$'), payload=payloadList)
-                        for res in intruder.run():
-                            if self.exclude:
-                                if str(res[0]) in self.exclude:
-                                    continue
-                            print(f'{res[0]}\t{res[1]}\t{res[2]}\t{res[3]}')
-                            result.append(res)
+                        for payload in payloadList:
+                            if self.blocks == 0:
+                                # We're out of luck, exit
+                                break
+                            user_agent = choice(self.useragents)
+                            headers = {'User-Agent': user_agent}
+                            payload = payload.strip('\n')
+                            url = baseurl.replace(value, payload)
+                            try:
+                                r = requests.get(url, headers=headers, allow_redirects=self.redir, proxies=self.proxy, verify=False)
+                                # Here we attempt to circumvent rate limit and/or temporary blocks on our IP
+                                # Often when 403 og 429 is returned because of a block a small html document
+                                # is returned with that is len =~ 200
+                                if r.status_code == 403 and len(r.content) < 200 or r.status_code == 429 and len(r.content) < 200:
+                                    # Testing has shown that at least blocks from Akamai last around 2 minutes
+                                    print(f"{str(datetime.now())} - Request was possibly blocked by WAF: Sleeping thread for 140 seconds for {self.target}")
+                                    sleep(140)
+                                    r = requests.get(url, headers=headers, allow_redirects=self.redir, proxies=self.proxy, verify=False)
+                                    if r.status_code == 403 and len(r.content) < 200 or r.status_code == 429 and len(r.content) < 200:
+                                        # At this point we just continue
+                                        self.blocks -= 1
+                                        continue
+                            except (SSL.SysCallError, gaierror):
+                                continue
+                            result.append([r.status_code, len(r.content), str(r.elapsed.total_seconds()*1000)[:7], url])
+                            print(f'{r.status_code}\t{len(r.content)}\t{str(r.elapsed.total_seconds()*1000)[:7]}\t{url}')
+                            if r.status_code == 404:
+                                # The url/endpoint is not working, lets just break out
+                                break
+                            '''if self.save_responses and len(r.content) != 0:
+                                try:
+                                    with open(f'{self.output_dir}/{str(datetime.now()).replace(" ", "T").replace(":", "-").split(".")[0]}-{self.payloaddata.index(payload)}', 'w') as f:
+                                        f.write(f'{url}\n\n{r.status_code}\n\n{r.content}')
+                                except:
+                                    print(f'Error: could not write file {self.output_dir}/{str(datetime.now()).replace(" ", "T").replace(":", "-").split(".")[0]}-{self.payloaddata.index(payload)}')'''
+                            sleep(0.5)                
         return result
